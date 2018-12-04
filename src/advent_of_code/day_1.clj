f76ffee05fa62483e2ba8ecead02cb0d4f656c07
Arne Schlüter <arne@schlueter.is>
2018-12-04 08:01:59 +0100

    Initial commit: Day 1

---
+++ src/advent_of_code/day_1.clj
@@ -0,0 +1,24 @@
+(ns advent-of-code.day-1
+  (:require [clojure.string :as str]))
+
+;; this is developed in emacs w/ cider
+;; the result comes from cider-pprint-eval-last-sexp-to-comment
+
+;; finding the final frequency
+
+(def shifts (->> (slurp "resources/input_1.txt")
+                 (str/split-lines)
+                 (map #(Long/parseLong %))))
+
+(reduce + shifts) ;; => 490
+
+;; finding the first duplicate frequency
+
+(loop [frequency 0
+       remaining (cycle shifts)
+       frequencies #{}]
+  (if (frequencies frequency)
+    frequency
+    (recur (+ frequency (first remaining))
+           (rest remaining)
+           (conj frequencies frequency)))) ;; => 70357
