2bd7e30c69f62ebab972f04ed52129d356403cad
Arne Schlüter <arne@schlueter.is>
2018-12-04 11:06:46 +0100

    Day 3: Add solution for part 2

--- src/day_3.clj
+++ src/day_3.clj
@@ -22,10 +22,14 @@
              y (range y (+ y h))]
          [x y])))
 
-(defn coord-map [claims]
+(defn claim->coords [claim]
+  (into {} (for [coord (space-of-claim claim)] [coord 1])))
+
+(defn coord-map
+  "Gives us a map of all occupied coordinates given a sequence of claims"
+  [claims]
   (r/reduce (fn [prev c]
-              (merge-with + prev (into {} (for [coord (space-of-claim c)]
-                                            [coord 1]))))
+              (merge-with + prev (claim->coords c)))
             {} claims))
 
 (defn total-overlap [claims]
@@ -49,3 +53,27 @@
 
 (total-overlap claims)
 ;; => 118539
+
+;; What is the one claim that is not overlapped?
+
+(let [#_#_ claims (map str->claim (str/split-lines example))
+      ;; use this for faster (constant instead of linear) lookup
+      coords->claim (->>
+                     (map (fn [c]
+                            [[(:x c) (:y c)] c])
+                          claims)
+                     (into {}))
+      ;; coords only occupied by one claim
+      sparse-coords (->> (filter #(= 1 (val %)) (coord-map claims))
+                         (into {}))]
+  ;; now for all coordinates that are occupied by only one claim,
+  ;; we check the claim that fits into these coordinates entirely
+  (->> (keep (fn [[coords _]]
+               (when-let [claim (coords->claim coords)]
+                 (when (->> (claim->coords claim)
+                            (map (fn [[coords _]] coords))
+                            (every? sparse-coords))
+                   claim)))
+             sparse-coords)
+       (first)))
+;; => {:id 1270, :x 221, :y 290, :w 24, :h 16}
