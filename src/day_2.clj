41695d139926bc8207cdb5efe4a5f663140a4cc5
Arne Schlüter <arne@schlueter.is>
2018-12-04 08:44:16 +0100

    Day 2 solved

---
+++ src/day_2.clj
@@ -0,0 +1,32 @@
+(ns day-2
+  (:require [clojure.string :as str]))
+
+(def input (->> (slurp "resources/input_2.txt")
+                (str/split-lines)))
+
+(defn checksum [box-ids]
+  (let [[twos threes] (->> (map frequencies box-ids)
+                           ;; keep all frequency maps that contain either 2 or 3
+                           ;; and keep them exactly once
+                           (mapcat #(keep (set (vals %)) [2 3]))
+                           (sort)
+                           ;; after this we end up with a list like ((2 2 2 2)
+                           ;; (3 3 3 3 3)) and count that
+                           (partition-by identity))]
+    (* (count twos) (count threes))))
+
+(checksum input)
+;; => 6888
+
+(defn common-letters [a b]
+  (remove nil? (map #(when (= %1 %2) %1) a b)))
+
+(->> (mapcat (fn [a]
+               (for [i input]
+                 (let [common (common-letters a i)]
+                   (when (= (count common) (dec (count a)))
+                     common))))
+             input)
+     (remove nil?)
+     (first))
+;; => (\i \c \x \j \v \b \r \o \b \t \u \n \l \e \l \z \p \d \m \f \k \a \h \g \s)
